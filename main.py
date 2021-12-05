40579d4e9375ad5bd626a08aa527aa46e49133dd
Martadiggs <marta.digaetano@student.h-farm.com>
2021-12-05 11:53:23 +0100

    aggiornamento textblob e argparce
    sistemato

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from lyrics import get_lyric
+from textblob import TextBlob
+import argparse as ap
 
-artist = "Beatles"
-title= "Across the Universe"
 
-song = get_lyric(artist, title)
+parser = ap.ArgumentParser()
+parser.add_argument("artist", help="First artist", type= str)
+parser.add_argument("song", help="Song of the first artist", type= str)
+args = parser.parse_args()
+artist=args.artist
+song=args.song
 
-print("{} by {}:".format(title, artist))
+song = get_lyric(artist, song)
+
+print("{} by {}:".format(song, artist))
 print("{}".format(song))
+
+
+text1= artist + song
+lan1= TextBlob(text1)
+print("the language of the song is:", lan1.detect_language())
