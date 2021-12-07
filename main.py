34b84ffb3546d768b6cb4afd6e14d29208d455d5
Martadiggs <marta.digaetano@student.h-farm.com>
2021-12-07 10:03:52 +0100

    textblob + argparce
    still not working

--- main.py
+++ main.py
@@ -14,37 +14,47 @@ from textblob import TextBlob
 import csv_file
 import argparse as ap
 
+#pip install -U textblob
+#python -m textblob.download_corpora
 
 csv_path = 'reader.csv'
 
 
-#pip install -U textblob
-#python -m textblob.download_corpora
 
-parser = ap.ArgumentParser(description = "the program" +
+
+parser = ap.ArgumentParser(description = "the program " +
                                         "let you put the name "+
                                         "of an artist and his song "+
-                                        "in order to retrieve the"+
-                                        "correspondant lyric..."+
-                                        "please wrap the argument"+
-                                        "around quotes")
+                                        "in order to retrieve the "+
+                                        "correspondant lyric... "+
+                                        "please wrap the argument "+
+                                        "around quotes ")
 parser.add_argument("artist", help="Please put the name of the artist", type= str)
 parser.add_argument("title", help="Please put the name of a song of the artist", type= str)
+
 args = parser.parse_args()
 artist=args.artist
 title=args.title
 
-song = get_lyric(artist, title) #lyric function to print the text of the selected song
-
+#lyric function to print the text of the selected song
+song = get_lyric(artist, title)
 print("{} by {}:".format(title, artist))
 print("{}".format(song))
-#print(type(song)) we checked wehter the lyric was in form of a string or not to see if the
+#we checked wether the lyric was in form of a string or not to see if the
 
 
 #use textblob library and detect_language function to determine the language of the song
 text1= artist + title
-lan1= TextBlob(text1)
+lan1= TextBlob(song)
 print("the language of the song is:", lan1.detect_language())
+
+parser.add_argument("-l", "--like", action="append", help="add a new song to playlist")
+args = parser.parse_args()
+like=args.like
+
+if args.like:
+    print("add to likes")
+
 """
 return the lyric of the last song you searched for
 """
