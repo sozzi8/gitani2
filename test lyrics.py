782488a68346c7204cdc58d4b25b3f68a3c6e574
marijasole <mariasolelanza@gmail.com>
2021-12-10 12:52:02 +0100

    test lyrics
    test lyrics.py, at the moment only pass the first test with correct values

---
+++ test lyrics.py
@@ -0,0 +1,43 @@
+import unittest
+import sys
+import os
+
+# add parent folder to path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from lyrics import get_lyric
+
+
+class TestInput(unittest.TestCase):
+
+    # smoke test: valid inputs
+    def test_correct_values(self):
+        artisttest= "Vasco Rossi"
+        titletest= "Vivere"
+        # you should select some valid inputs, for which the output is known
+        self.assertEqual(get_lyric(artisttest, titletest),get_lyric("Vasco Rossi","Vivere"))
+
+    # invalid inputs
+    def test_wrong_values(self):
+        artisttest= "Vasco Rossi"
+        titletest= "Vivvvere"
+        artisttest2= "Vasso Rose"
+        titletest2= "Vivere"
+        # you should input wrong data
+        self.assertEqual(get_lyric(artisttest,titletest), get_lyric(artisttest,titletest))
+        self.assertEqual(get_lyric(artisttest2,titletest2),get_lyric(artisttest2,titletest2) )
+
+        # NOTE: the following test passing an empty list will fail!
+        # self.assertEqual(return_birthday([]), None)
+
+    # corner case: empty string
+    def test_empty_string(self):
+        self.assertEqual(get_lyric("-", "Vivere"),get_lyric("", "Vivere"))
+
+
+if __name__ == '__main__':
+
+    # basic test
+    #unittest.main()
+
+    # with more details
+    unittest.main(verbosity=2)
