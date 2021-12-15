c242b07783506f4835757c633eb4dd50a21ff61e
Martadiggs <marta.digaetano@student.h-farm.com>
2021-12-15 17:33:41 +0100

    update comments csv

--- csv_playlist.py
+++ csv_playlist.py
@@ -2,12 +2,13 @@ import csv
 from lyrics import get_lyric
 import pandas as pd
 
+
+"""
+the following function adds the artist and title
+selected by the user to the song.csv file in order
+to create the user's playlist
+"""
 def database_of_songs(artist, title, name_csv="songs.csv"):
-    """
-    the following function takes the elements present
-    in the dictionary and places them into a csv file
-    named songs.csv that we will use as our playlist.
-    """
 
     with open('songs.csv','a') as fd:
         fd.write(title + ","+ artist + "\n")
