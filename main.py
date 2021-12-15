fb26f1692397c73651e18afbeace99484e867558
Edoardo Sozzi <soed1600@gmail.com>
2021-12-15 16:30:21 +0100

    proveandateamale

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ try:
     print("{} by {}:".format(title, artist))
     print("{}".format(song))
 except KeyError:
-    print("ERROR: something went worng!")
+    print("ERROR: something went wrong!")
     print("please try again!")
     sys.exit()
 # we checked wether the lyric was in form of a string or not to see if the
