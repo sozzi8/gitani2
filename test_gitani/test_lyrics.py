440f43dbf834f6bd0489c6dd5a38e9f6bc1accfb
marijasole <mariasolelanza@gmail.com>
2021-12-17 15:09:58 +0100

    Revert "sistemati test"
    This reverts commit 592cee0e5f31115039730bd3d1812e05461f7879.

--- test_gitani/test_lyrics.py
+++ test_gitani/test_lyrics.py
@@ -1,46 +1,51 @@
-from lyrics import get_lyric
 import unittest
 import sys
 import os
 
 # add parent folder to path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from lyrics import get_lyric
 
-
+from csv_file import write_data
 class TestInput(unittest.TestCase):
 
     # smoke test: valid inputs
     def test_correct_values(self):
-        artisttest = "Vasco Rossi"
-        titletest = "Vivere"
+        artisttest= "Vasco Rossi"
+        titletest= "Vivere"
         # you should select some valid inputs, for which the output is known
-        self.assertEqual(get_lyric(artisttest, titletest),
-                         get_lyric("Vasco Rossi", "Vivere"))
+        self.assertEqual(get_lyric(artisttest, titletest),get_lyric("Vasco Rossi","Vivere"))
 
-    # invalid inputs
-    def test_wrong_values(self):
-        artisttest = "Vasco Rossi"
-        titletest = "Vivvvere"
-        artisttest2 = "Vasso Rose"
-        titletest2 = "Vivere"
+
+
+    # edge inputs (strange)
+    def test_edgy_values(self):
+        artisttest1= "VASCO ROSSI"
+        titletest1= "VIVERE"
+        artisttest2= "VaScO RosSi"
+        titletest2= "ViVEre"
         # you should input wrong data
-        self.assertRaises(KeyError, get_lyric, artisttest, titletest)
-        self.assertRaises(KeyError, get_lyric, artisttest2, titletest2)
+        self.assertEqual(get_lyric(artisttest1,titletest1),get_lyric(artisttest1,titletest1))
+        self.assertEqual(get_lyric(artisttest2,titletest2),get_lyric(artisttest2,titletest2) )
 
         # NOTE: the following test passing an empty list will fail!
         # self.assertEqual(return_birthday([]), None)
 
     # corner case: empty string
+#    def test_wrong_string(self):
+        #artisttest3= "- "
+        #titletest3= "Vivere"
+        #self.assertFalse(get_lyric(artisttest3,titletest3),get_lyric( artisttest3, titletest3))
 
-    def test_empty_string(self):
-        self.assertRaises(KeyError, get_lyric, "-", "Vivere")
-        self.assertRaises(KeyError, get_lyric, " ", "Vivere")
+
+
+
 
 
 if __name__ == '__main__':
 
     # basic test
-    # unittest.main()
+    #unittest.main()
 
     # with more details
     unittest.main(verbosity=2)
--- test_gitani/test_playlist.py
+++ test_gitani/test_playlist.py
@@ -1,44 +1,39 @@
 import unittest
 import sys
 import os
-from csv_playlist import database_of_songs
-import csv_playlist
 #from lyrics import get_lyric
 
 
 # add parent folder to path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from lyrics import get_lyric
 
 class TestInput(unittest.TestCase):
-
     # smoke test: valid inputs
-    def test_correct_values(self):
+    def Test_correct(self):
         # you should select some valid inputs, for which the output is known
-        self.assertEqual(database_of_songs("vasco rossi", "vivere"),
-                         print("you have added this song to your playlist"))
+        self.assertEqual(get_lyric("Vasco Rossi", "Vivere", "-l", "yes"),get_lyric("Vasco Rossi","Vivere","-l", "yes"))
+        #self.assertEqual(get_lyric("vasco rossi" "vivere" "-l" "yes"), (get_lyric("vasco rossi" "vivere" "-l" "yes"))
 
     # invalid inputs
-    def test_wrong_values(self):
+    #def test_wrong_values(self):
         # you should input wrong data
-        self.assertEqual(database_of_songs("Ada Lovelae", "none"), print(
-            "you have added this song to your playlist"))
-        self.assertEqual(database_of_songs("gross", "live"), print(
-            "you have added this song to your playlist"))
+        #self.assertFalse(get_lyric("Vasco Rossi", "Vivere", "-L", "YES"), get_lyric("Vasco Rossi", "Vivere","-L" "YES"))
+        #self.assertEqual(return_birthday(7), None)
 
         # NOTE: the following test passing an empty list will fail!
-        # self.assertEqual(database_of_songs([]), None)
+        # self.assertEqual(return_birthday([]), None)
 
     # corner case: empty string
-    def test_empty_string(self):
-        self.assertEqual(database_of_songs(" ", " "), print(
-            "you have added this song to your playlist"))
+#    def test_empty_string(self):
+    #    self.assertEqual(return_birthday(""), None)
 
 
 if __name__ == '__main__':
 
     # basic test
-    # unittest.main()
+    #unittest.main()
 
     # with more details
     unittest.main(verbosity=2)
