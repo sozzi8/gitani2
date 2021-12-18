0b248661e2a4fc5b0f26417cb66d1b99b6938c72
Martadiggs <marta.digaetano@student.h-farm.com>
2021-12-18 11:04:09 +0100

    test_lastsong pep8 e comments

--- test_lastsong.py
+++ test_lastsong.py
@@ -5,25 +5,30 @@ import pandas as pd
 import unittest
 import sys
 import os
+import pycodestyle
 
-# add parent folder to path
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 class TestInput(unittest.TestCase):
 
+    # test valid inputs
     def test_correct_values_last(self):
         artisttest9 = "Vasco Rossi"
         titletest9 = "Vivere"
-        marta = get_lyric(artisttest9, titletest9)
-        # you should select some valid inputs, for which the output is known
-        self.assertEqual(write_data("test.csv", artisttest9, titletest9, marta),
-                         write_data("test.csv", artisttest9, titletest9, marta))
+        canzone = get_lyric(artisttest9, titletest9)
+        self.assertEqual(write_data("test.csv", artisttest9,
+                         titletest9, canzone),
+                         write_data("test.csv",
+                         artisttest9, titletest9, canzone))
 
+    # test with non existent file
     def test_with_nonexistent_file(self):
         path = "NONE.csv"
         self.assertEqual(read_data(path), print('The file does not exist'))
 
+    # test with wrong encoding
     def test_with_wrongencoding(self):
         path = "reader.csv"
         self.assertEqual(read_data(path), print('File has a wrong encoding'))
